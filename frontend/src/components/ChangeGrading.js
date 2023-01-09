28d9869d8c4613805af6d247c5d92a61312d6526
Martin "Atmaak" Kubjak <77574694+Atmaak@users.noreply.github.com>
2023-01-09 12:00:19 +0100

    Update ChangeGrading.js

--- frontend/src/components/ChangeGrading.js
+++ frontend/src/components/ChangeGrading.js
@@ -13,43 +13,33 @@ const AddGrading = () => {
   const [pieChartData, setPieChartData] = useState([])
   const [grades, setGrades] = useState()
 
-  const one = useRef('80')
-  const two = useRef('60')
-  const three = useRef('40')
-  const four = useRef('30')
-  const five = useRef('20')
+  const one = /* useRef() */ createRef()
+  const two = /* useRef() */ createRef()
+  const three = /* useRef() */ createRef()
+  const four = /* useRef() */ createRef()
+  const five = /* useRef() */ createRef()
   
   useEffect(() => {
     if(!shownOwnTest) return history('/teacher')
     let _grades = shownOwnTest.test_grades
     const xdddddd= async () => {
       await setGrades(_grades)
-      console.log(grades)
+      console.log(_grades)
       setPieChartData([
-        { title: 'Jednička', value: parseFloat(100 - grades[0].percentage),range: `100% - ${grades[0].percentage}%`, color: '#1fea00' },
-        { title: 'Dvojka', value: parseFloat(grades[0].percentage - grades[1].percentage), range: `${grades[0].percentage}% - ${grades[1].percentage}%`, color: '#27a102' },
-        { title: 'Trojka', value: parseFloat(grades[1].percentage - grades[2].percentage), range: `${grades[1].percentage}% - ${grades[2].percentage}%`, color: '#1c6000' },
-        { title: 'Čtyřka', value: parseFloat(grades[2].percentage - grades[3].percentage), range: `${grades[2].percentage}% - ${grades[3].percentage}%`, color: '#002f06' },
-        { title: 'Pětka', value: parseFloat(grades[4].percentage - 0), range: `${grades[4].percentage} % - 0%`, color: '#000000' }
+        { title: 'Jednička', value: parseFloat(100 - _grades[0].percentage),range: `100% - ${_grades[0].percentage}%`, color: '#1fea00' },
+        { title: 'Dvojka', value: parseFloat(_grades[0].percentage - _grades[1].percentage), range: `${_grades[0].percentage}% - ${_grades[1].percentage}%`, color: '#27a102' },
+        { title: 'Trojka', value: parseFloat(_grades[1].percentage - _grades[2].percentage), range: `${_grades[1].percentage}% - ${_grades[2].percentage}%`, color: '#1c6000' },
+        { title: 'Čtyřka', value: parseFloat(_grades[2].percentage - _grades[3].percentage), range: `${_grades[2].percentage}% - ${_grades[3].percentage}%`, color: '#002f06' },
+        { title: 'Pětka', value: parseFloat(_grades[4].percentage - 0), range: `${_grades[4].percentage} % - 0%`, color: '#000000' }
       ])
-      one.current.value = grades[0].percentage
-      two.current.value = grades[1].percentage
-      three.current.value = grades[2].percentage
-      four.current.value = grades[3].percentage
-      five.current.value = grades[4].percentage
 
       setLoading(false)
     }
     xdddddd()
-  }, [])
+  }, [loading])
   
 
   const handleClick = () => {
-    console.log(one.current.value)
-    console.log(two.current.value)
-    console.log(three.current.value)
-    console.log(four.current.value)
-    console.log(five.current.value)
     if(one.current.value <= 0 || 100 < one.current.value) return setErr('Špatně zadána 1!')
     if(two.current.value <= 0 || one.current.value < two.current.value) return setErr('Špatně zadána 2!')
     if(three.current.value <= 0 || two.current.value < three.current.value) return setErr('Špatně zadána 3!')
@@ -62,14 +52,14 @@ const AddGrading = () => {
       { title: 'Pětka', value: parseFloat(five.current.value - 0), range: `${five.current.value}% - ${0}%`, color: '#000000' }
     ])
 
-    console.log(pieChartData)
+    
 
     addGrading([one.current.value, two.current.value, three.current.value, four.current.value, five.current.value])
     setErr('')
   }
   return (
     <div style={{minHeight: "95.5vh"}} className={`bg-${DarkMode}`}>
-    <Container className={`text-${textDarkMode}`}>
+    {!loading && <Container className={`text-${textDarkMode}`}>
       {pieChartData && <div className='d-flex w-100'>
         <div style={{minWidth: "500px", minHeight: "500px"}}>
           {pieChartData.map(data => (
@@ -86,32 +76,32 @@ const AddGrading = () => {
         data={pieChartData}
       />
       </div>}
-      {!loading && <Form onSubmit={(e) => e.preventDefault()}>
+      <Form onSubmit={(e) => e.preventDefault()}>
         {err && <Alert variant="danger">{err}</Alert>}
         <Form.Group id="email" className="d-flex m-3">
           <Form.Label className="mx-3">1: </Form.Label>
-          <Form.Control className="mx-3" type="number" ref={one} min={two.current.value} max={100} required />
+          <Form.Control className="mx-3" type="number" ref={one} min={grades[1].percentage} max={100} required />
         </Form.Group>
         <Form.Group id="email" className="d-flex m-3">
           <Form.Label className="mx-3">2: </Form.Label>
-          <Form.Control className="mx-3" type="number" ref={two} min={three.current.value} max={one.current.value} required />
+          <Form.Control className="mx-3" type="number" ref={two} min={grades[2].percentage} max={grades[0].percentage} required />
         </Form.Group>
         <Form.Group id="email" className="d-flex m-3">
           <Form.Label className="mx-3">3: </Form.Label>
-          <Form.Control className="mx-3" type="number" ref={three} min={four.current.value} max={two.current.value} required />
+          <Form.Control className="mx-3" type="number" ref={three} min={grades[3].percentage} max={grades[1].percentage} required />
         </Form.Group>
         <Form.Group id="email" className="d-flex m-3">
           <Form.Label className="mx-3">4: </Form.Label>
-          <Form.Control className="mx-3" type="number" ref={four} min={five.current.value} max={three.current.value} required />
+          <Form.Control className="mx-3" type="number" ref={four} min={grades[4].percentage} max={grades[2].percentage} required />
         </Form.Group>
         <Form.Group id="email" className="d-flex m-3">
           <Form.Label className="mx-3">5: </Form.Label>
-          <Form.Control className="mx-3" type="number" ref={five} min={0} max={four.current.value} required />
+          <Form.Control className="mx-3" type="number" ref={five} min={0} max={grades[3].percentage} required />
         </Form.Group>
         <Button onClick={handleClick} type="submit" className='w-100 m-3' variant={textDarkMode}>Přidat známkování</Button>
-    </Form>}
+    </Form>
   
-    </Container>
+    </Container>}
     </div>
     )
 }
